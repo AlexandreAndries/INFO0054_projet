736e19d5428760ab48e564d0b3a6e2f3c722a740
ThomasRotheudt <thomas.rotheudt@student.uliege.be>
2021-11-25 14:15:16 +0100

    MàJ

--- Semtab.rkt
+++ Semtab.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
 
-(define (semtab formule_prepositionnel)
-  
+(define (semtab formule_propositionnel)
+  (cond (null? formule_propositionnel) (display "Pas de formule propositionnel"))
 )
