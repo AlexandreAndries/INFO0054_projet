8f798abe41694e876655b8bd80345979a18a9417
ThomasRotheudt <thomas.rotheudt@student.uliege.be>
2021-11-30 15:51:22 +0100

    Légère progression

--- Semtab.rkt
+++ Semtab.rkt
@@ -1,6 +1,54 @@
+
 #lang racket
+
+;  table de vérité de OR
+;    ___________________
+;   | p    q |  (OR p q)|
+;   | #f   #f|     #f   |
+;   | #f   #t|     #t   |
+;   | #t   #f|     #t   |
+;   | #t   #t|     #t   |
+
+;  table de vérité de AND
+;    ____________________
+;   | p    q |  (AND p q)|
+;   | #f   #f|     #f    |
+;   | #f   #t|     #f    |
+;   | #t   #f|     #f    |
+;   | #t   #t|     #t    |
+
+;  table de vérité de IFTHEN
+;    _______________________
+;   | p    q |  (IFTHEN p q)|
+;   | #f   #f|     #t       |
+;   | #f   #t|     #t       |
+;   | #t   #f|     #f       |
+;   | #t   #t|     #t       |
+
+;  table de vérité de NOT
+;    _____________
+;   |  p | (NOT p)|
+;   | #f |   #t   |
+;   | #t |   #f   |
+    
+(define p #t)
+(define q #f)
+(define r #t)
+
+;Vérifie si la liste x est un atome
+;   précondition: x != null
+;
+;   retourne: 
+;         #t si x est un atome
+;         #f sinon
+(define (atom? x)
+  (and (not (null? x))
+       (not (pair? x))))
 
 
 (define (semtab formule_propositionnel)
-  (cond (null? formule_propositionnel) (display "Pas de formule propositionnel")
+  (cond ((null? formule_propositionnel) (display "Aucune formule propositionnel\n"))
+  )
 )
+
+(semtab '())
--- logic_lib.rkt
+++ logic_lib.rkt
@@ -1,19 +1,25 @@
 #lang racket
 
 (define (satisfiable? formule_propositionnel)
+  '()
 )
 
 (define (valid? formule_propositionnel)
+  '()
 )
 
 (define (tautology? formule_propositionnel)
+  '()
 )
 
 (define (contradiction? formule_propositionnel)
+  '()
 )
 
 (define (models formule_propositionnel)
+  '()
 )
 
 (define (counterexamples formule_propositionnel)
+  '()
 )
