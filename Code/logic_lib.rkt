ec43762ae08e56d16adc8ec5fe357a72b5f8ec0e
ThomasRotheudt <thomas.rotheudt@student.uliege.be>
2021-12-17 14:52:53 +0100

    Améliorations

--- Code/logic_lib.rkt
+++ Code/logic_lib.rkt
@@ -1,13 +1,37 @@
 #lang racket
+; ---------------------------------------------------------------------------- ;
+; -----------------------------------SEMTAB.RKT------------------------------- ;
+; ---------------------------------------------------------------------------- ;
+; # Auteurs : ROTHEUDT Thomas - ANDRIES Alexandre - KHALIPHI Abdelilah         ;
+; # Projet  : INFO0054 - Programmation Fonctionelle                            ;
+; # Date    : 11/12/2021                                                       ;
+; ---------------------------------------------------------------------------- ;
+(require "tableau.rkt")
 (require "Semtab.rkt")
+; ---------------------------------------------------------------------------- ;
+; -----------------------Définition des tableaux de tests--------------------- ;
+; ---------------------------------------------------------------------------- ;
+(define test-tautology '(OR a (NOT a)))
+(define test-satisfiable '(a (NOT b) (IFTHEN (NOT b) c)))
+(define test-contradiction '(AND a (OR b (NOT a))))
+; ---------------------------------------------------------------------------- ;
+; --------------------------------FONCTION LOGIC------------------------------ ;
+; ---------------------------------------------------------------------------- ;
 
-
-(define test-semtab '((OR a (NOT a))))
-
-(define (satisfiable? formule) (contient? #f (map contient-contradiction? (elim-operation formule))))
-
-(define (tautology? formule) (not (contient? #f (map (lambda (x) (not (contient-contradiction? x))) (elim-operation formule)))))
-
-(define (contradiction? formule) (contient? #t (map (lambda (x) (contient-contradiction? x)) (elim-operation formule))))
-
-(contradiction? '(AND a (NOT a)))
+(define (satisfiable? liste-formule) (contient? #f (map contient-contradiction? (semtab liste-formule)))
+)
+; ---------------------------------------------------------------------------- ;
+(define (tautology? formule) (not (contient? #f (map (lambda (x) (not (contient-contradiction? x))) (semtab (cree-liste-tableau formule)))))
+)
+; ---------------------------------------------------------------------------- ;
+(define (contradiction? formule) (contient? #t (map (lambda (x) (contient-contradiction? x)) (semtab (cree-liste-tableau formule))))
+)
+; ---------------------------------------------------------------------------- ;
+; ---------------------------------------TEST--------------------------------- ;
+; ---------------------------------------------------------------------------- ;
+(tautology? test-tautology)                  ; doit donner #t 
+(satisfiable? test-satisfiable)              ; doit donner #t
+(contradiction? test-contradiction)          ; doit donner #t
+; ---------------------------------------------------------------------------- ;
+; -----------------------------------FIN TEST--------------------------------- ;
+; ---------------------------------------------------------------------------- ;
